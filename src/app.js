c393ed2fa29b162b4f0ed94f311840de9b180cec
Vivek Shukla <vivekshukla@Viveks-MacBook-Air.local>
2025-03-01 11:18:25 +0530

    Exploring get, post and delete https methods

--- src/app.js
+++ src/app.js
@@ -3,13 +3,35 @@ const express = require('express');
 const app = express();
 
 // This is request handler
+app.use("/user",(req, res) => {
+    res.send("Hello Vivek");
+});
+
+// this will match to only get HTTP method API calls to /user
+app.get("/user",(req,res) => {
+    res.send({
+        name: "Vivek",
+        age: 22
+    });
+});
+
+app.post("/user",(req,res) => {
+    res.send("Data saved successfully");
+});
+
+app.delete("/user",(req,res) => {
+    res.send("Data deleted successfully");
+});
+
+// this will match to all the HTTP method API calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello from the server!");
-})
+});
 
 app.use("/hello",(req, res) => {
     res.send("Hello from the hello1!");
-})
+});
+
 
 app.listen(7777, () => {
     console.log("Server is running on port 7777");
